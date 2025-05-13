e7088ef069ab40be273c108482325927f99c0a98
shaay98 <nrichardson567@icloud.com>
2025-05-13 02:55:26 +0000

    sass styling first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+     <h1 className='section-one'>Sass Demo</h1>
+     <p className='section-two'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, doloribus. Numquam obcaecati dignissimos in autem! Sunt sint magni officia aut, qui error ratione distinctio illum tempora minus eaque porro corrupti?</p>
+     <footer className='section-three'>Copyright &copy; 2025 Nijah Richardson</footer>
+    </>
+  )
+}
+
+export default App
